f9053bd6eb5f1fc2763015bdb58400e3aab0612b
amireinidavid <realheritagecompany@gmail.com>
2025-01-02 11:15:19 +0100

    Add PlaidLink component and integrate with AuthForm; initialize Plaid API client

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,7 @@ import { Loader2 } from "lucide-react";
 import { authFormSchema } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -83,7 +84,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink />
+        </div>
       ) : (
         <>
           <Form {...form}>
---
+++ components/PlaidLink.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaidLink = () => {
+  return <div></div>;
+};
+
+export default PlaidLink;
